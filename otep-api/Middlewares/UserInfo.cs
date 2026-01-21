cd5b2a452e766630ff3548c4ec558afba4f0c9ea
pjames <pjames@gmail.com>
2026-01-21 10:57:23 +0700

    Added audit log

---
+++ otep-api/Middlewares/UserInfo.cs
@@ -0,0 +1,11 @@
+namespace Its.Otep.Api.AuditLogs
+{
+    public class UserInfo
+    {
+        public string? Role { get; set; }
+        public string? CustomRole { get; set; }
+        public string? IdentityType { get; set; }
+        public string? UserId { get; set; }
+        public string? UserName { get; set; }
+    }
+}
